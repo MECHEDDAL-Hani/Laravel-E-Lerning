152281396a438077fa6c0e392d19703b6182035d
Hani <hanimecheddal@gmail.com>
2020-06-21 09:07:09 +0200

    Design Pages
    - Welcome Page.
    - Login Page.
    - Register Page.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+<section class="h-100 my-login-page">
+    <div class="container h-100">
+        <div class="row justify-content-md-center h-100">
+            <div class="card-wrapper">
+                <div class="brand">
+                    <img src="{{ asset('img/P4.png') }}" alt="logo">
+                </div>
+                <div class="card fat">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">Register</h4>
+                        <form method="POST" class="my-login-validation" novalidate="" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                                    name="name" required autofocus value="{{ old('name') }}" autocomplete="name">
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div class="invalid-feedback" role="alert"> {{ $message }} </div>
                                 @enderror
+                                <div class="invalid-feedback">
+                                    What's your name?
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                            <div class="form-group">
+                                <label for="email">E-Mail Address</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email">
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div class="invalid-feedback" role="alert"> {{ $message }} </div>
                                 @enderror
+                                <div class="invalid-feedback">
+                                    Your email is invalid
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required data-eye autocomplete="new-password">
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <div class="invalid-feedback" role="alert"> {{ $message }} </div>
                                 @enderror
+                                <div class="invalid-feedback">
+                                    Password is required
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="form-group">
+                                <label for="password">Confirm Password</label>
+                                <input id="password-2" type="password" class="form-control" name="password_confirmation"
+                                    required data-eye>
+                                <div class="invalid-feedback">
+                                    Password is required
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                            <div class="form-group m-0">
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    Register
                                 </button>
                             </div>
-                        </div>
-                    </form>
+                            <div class="mt-4 text-center">
+                                Already have an account? <a href="{{ route('login') }}">Login</a>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,65 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+    @extends('layouts.app')
+    @section('content')
+    <header id="header" class="header-section">
+        <div class="container  font-weight-bold mon-nav">
+            <nav class="navbar navbar-expand-lg navbar-light bg-primary ">
+                <a class="navbar-brand text-white" href="{{ url('/') }}">
+                    <img src="{{ asset('img/P0.png') }}" width="30" height="30" class="d-inline-block align-top"
+                        alt="E-Lerning">
+                    <span>E-Lerning</span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">&nbsp;</span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active"><a class="nav-link text-white" href="#">Cources</a></li>
+                        <li class="nav-item active"><a class="nav-link text-white" href="#">Teachers</a></li>
+                        @auth
+                        <li class="nav-item active"><a class="nav-link text-white" href="{{ url('/home') }}">Home</a>
+                        </li>
+                        @else
+                        <li class="nav-item active"><a class="nav-link text-white" href="{{ route('login') }}">Login</a>
+                        </li>
+                        <li class="nav-item active"><a class="nav-link text-white"
+                                href="{{ route('register') }}">Register</a></li>
+                        @endauth
+                    </ul>
                 </div>
-            @endif
+            </nav>
+        </div>
+    </header>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+    <div class="container">
+        <section id="Imag-slid" class="carousel slide row" data-ride="carousel">
+            <div class="carousel-inner ">
+                <div class="carousel-item active">
+                    <img class="d-block img-fluid w-100" src="{{ asset('img/P1.png') }}" alt="E-Lerning Image 01">
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid w-80" src="{{ asset('img/P2.png') }}" alt="E-Lerning Image 02">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block img-fluid w-80" src="{{ asset('img/P3.png') }}" alt="E-Lerning Image 03">
                 </div>
             </div>
-        </div>
-    </body>
-</html>
+            <ol class="carousel-indicators">
+                <li data-target="#Imag-slid" data-slide-to="0" class="active">&nbsp;</li>
+                <li data-target="#Imag-slid" data-slide-to="1">&nbsp;</li>
+                <li data-target="#Imag-slid" data-slide-to="2">&nbsp;</li>
+            </ol>
+        </section>
+        <section class="page-section bg-primary text-white row">
+            <div class="card-body text-center">
+                <h3>E-lerning</h3>
+                <h5 class="card-title">New Educational Technology</h5>
+                <p class="lead card-text">Used Technologies For Learning And Self Development.</p>
+            </div>
+        </section>
+    </div>
+    <div class="copyright container py-4 text-center text-white">
+        <div class="container"><small>Copyright © Your Website 2020</small></div>
+    </div>
+    @endsection
