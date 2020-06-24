1f1a0aba56220ae7690b549367ecdac23765dc4c
Hani <hanimecheddal@gmail.com>
2020-06-24 18:22:52 +0200

    Creat Admin
    php artisan make:model Model\Admin -m -r
    -Model
    -Migration
    -Controller

--- database/migrations/2020_06_24_161703_create_admins_table.php
+++ database/migrations/2020_06_24_161703_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTeachersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateTeachersTable extends Migration
      */
     public function up()
     {
-        Schema::create('teachers', function (Blueprint $table) {
-            
-            $table->unsignedBigInteger('student_id');
+        Schema::create('admins', function (Blueprint $table) {
+        
+            $table->unsignedBigInteger('teacher_id');
             $table->timestamps();
-            $table->primary('student_id');
-            $table->foreign('student_id')->references('user_id')->on('students')->onDelete('cascade');
+            $table->primary('teacher_id');
+            $table->foreign('teacher_id')->references('student_id')->on('teachers')->onDelete('cascade');
         
         });
     }
@@ -30,6 +30,6 @@ class CreateTeachersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('teachers');
+        Schema::dropIfExists('admins');
     }
 }
