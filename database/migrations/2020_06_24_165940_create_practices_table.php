0592dc72f1e54707b834b6f3b06ce0f44cf56cff
Hani <hanimecheddal@gmail.com>
2020-06-24 19:04:12 +0200

    Creat Practice
    php artisan make:model Model\Practice -m -r
    -Model
    -Migration
    -Controller

---
+++ database/migrations/2020_06_24_165940_create_practices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePracticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('practices', function (Blueprint $table) {
+           
+            $table->unsignedBigInteger('resource_id');
+            $table->timestamps();
+            $table->primary('resource_id');
+            $table->foreign('resource_id')->references('id')->on('resources')->onDelete('cascade');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('practices');
+    }
+}
