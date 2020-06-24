0053e58160b19353edd95d96267ea80cc9c4aa03
Hani <hanimecheddal@gmail.com>
2020-06-24 19:24:18 +0200

    Creat Exercise
    php artisan make:model Model\Exercise -m -r
    -Model
    -Migration
    -Controller

---
+++ database/migrations/2020_06_24_172304_create_exercises_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExercisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('practice_id');
+            $table->timestamps();
+            $table->primary('practice_id');
+            $table->foreign('practice_id')->references('resource_id')->on('practices')->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercises');
+    }
+}
