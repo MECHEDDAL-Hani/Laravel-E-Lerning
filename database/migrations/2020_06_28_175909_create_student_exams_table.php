3e00fd0dbb1c8b8839bf23234d1d378b58cd20f2
Hani <hanimecheddal@gmail.com>
2020-06-28 20:01:58 +0200

    Creat StudentExam
    php artisan make:model Model\StudentExam -m -r
    -Model
    -Migration
    -Controller

---
+++ database/migrations/2020_06_28_175909_create_student_exams_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_exams', function (Blueprint $table) {
+           
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('exercise_id');
+            $table->unsignedBigInteger('reponce_id');
+            $table->integer('note')->default(0);
+            $table->timestamps();
+            $table->primary(['student_id', 'exercise_id', 'reponce_id']);
+            $table->foreign('student_id')->references('user_id')->on('students');
+            $table->foreign('exercise_id')->references('practice_id')->on('exercises');
+            $table->foreign('reponce_id')->references('id')->on('reponces');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_exams');
+    }
+}
