a0d9a0b8a3ac0ac334ac41aae9b306fed2f46db3
Hani <hanimecheddal@gmail.com>
2020-06-28 19:58:25 +0200

    Creat StudentExercise
    php artisan make:model Model\StudentExercise -m -r
    -Model
    -Migration
    -Controller

---
+++ app/Http/Controllers/StudentExerciseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\StudentExercise;
+use Illuminate\Http\Request;
+
+class StudentExerciseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\StudentExercise  $studentExercise
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StudentExercise $studentExercise)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\StudentExercise  $studentExercise
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StudentExercise $studentExercise)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\StudentExercise  $studentExercise
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StudentExercise $studentExercise)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\StudentExercise  $studentExercise
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StudentExercise $studentExercise)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_06_28_172158_create_student_exercises_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentExercisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_exercises', function (Blueprint $table) {
+           
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('exercise_id');
+            $table->unsignedBigInteger('reponce_id'); 
+            $table->timestamps();
+            $table->primary(['student_id', 'exercise_id' , 'reponce_id']);
+            $table->foreign('student_id')->references('user_id')->on('students');
+            $table->foreign('exercise_id')->references('practice_id')->on('exercises');
+            $table->foreign('reponce_id')->references('id')->on('reponces');
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_exercises');
+    }
+}
